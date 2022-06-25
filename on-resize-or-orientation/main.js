3b3eee2de58bdd335f657cf94473929c8fb63ed3
yajamon <yajamon.tatsuki@gmail.com>
2022-06-26 03:49:02 +0900

    defer化によってDOMContentLoadedを待たなくてよい

--- on-resize-or-orientation/main.js
+++ on-resize-or-orientation/main.js
@@ -2,7 +2,6 @@ function createNodesBy(text) {
   return text.split("\n").map((line) => document.createTextNode(line));
 }
 function put(text) {
-  var target = document.getElementById("print");
   createNodesBy(text).forEach((current) => {
     target.appendChild(current);
     target.appendChild(document.createElement("BR"));
@@ -14,8 +13,7 @@ function printWindowSize(event) {
       " Height: " + window.innerHeight,
   );
 }
-window.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("resize", printWindowSize);
-  window.addEventListener("orientationchange", printWindowSize);
-});
+const target = document.getElementById("print");
+window.addEventListener("resize", printWindowSize);
+window.addEventListener("orientationchange", printWindowSize);
 put("リサイズしたり、端末を回転させたりすると情報が出力されます。");
