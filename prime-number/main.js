6deb3a23aa95672ac84d9c01ee91bbe3635c53ba
yajamon <yajamon.tatsuki@gmail.com>
2022-06-25 20:06:15 +0900

    素数を判定するスクリプトを作る
    ざっくり実装

---
+++ prime-number/main.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("in");
+const result = document.getElementById("result");
+
+input.addEventListener("change", () => {
+  const value = Math.floor(parseInt(input.value, 10));
+  if (isPrimeNumber(value)) {
+    result.innerText = "素数です。";
+  } else {
+    result.innerText = "素数ではありません。";
+  }
+});
+
+function isPrimeNumber(value) {
+  let element = 2;
+  while (element * element <= value) {
+    if (value % element === 0) {
+      return false;
+    }
+    element += 1;
+  }
+  return true;
+}
